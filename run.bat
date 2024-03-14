8254a25d7cd0d0aa45b814b129eda03f86d48e68
Leon van Zyl <78896180+leonvanzyl@users.noreply.github.com>
2024-03-14 10:50:54 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,8 +2,8 @@
 call git clone https://github.com/FlowiseAI/Flowise.git
 call npm install --global yarn
 call cd Flowise
-call yarn upgrade
-call yarn install
-call yarn build
+call pnpm upgrade
+call pnpm install
+call pnpm build
 start http://localhost:3000
-call yarn start
+call pnpm start
